11c29b027a530a4362dca44ebdec79128aec4463
Rubens Matheus <112494717+RubensMatheus-me@users.noreply.github.com>
2024-07-09 21:03:23 -0300

    Update os.cpp
    commit 2

--- os.cpp
+++ os.cpp
@@ -29,29 +29,12 @@ void boot (Arch::Terminal *terminal, Arch::Cpu *cpu)
 // ---------------------------------------
 
 void interrupt (const Arch::InterruptCode interrupt){
-	int typed_char = terminalTeste->read_typed_char();
-	std::string x = "";
-	std::cin >> x;
+	
+	
 	if(interrupt == Arch::InterruptCode::Keyboard) {
-		terminalTeste->println(Arch::Terminal::Type::Command, x);
-		
-		if (terminalTeste->is_backspace(typed_char)) {
-			terminalTeste->println(Arch::Terminal::Type::Command, "apagar");
-		}
-		
-		else if (terminalTeste->is_alpha(typed_char)) {	
-			terminalTeste->println(Arch::Terminal::Type::Command, "letra");	
-		}
-		
-		else if (terminalTeste->is_num(typed_char)) {
-			terminalTeste->println(Arch::Terminal::Type::Command, "numero");
-		}
-		
-		else if (terminalTeste->is_return(typed_char)) {
-			terminalTeste->println(Arch::Terminal::Type::Command, "enter");
-		}
-		
-		
+		int typed_char = terminalTeste->read_typed_char();
+		char char_typed = static_cast<char>(typed_char);
+					
 	}
 }
 
