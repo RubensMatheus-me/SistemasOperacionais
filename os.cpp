424351efe9f66b467dfaaf3f54a4e53db1d3cb7e
Rubens Matheus <112494717+RubensMatheus-me@users.noreply.github.com>
2024-07-23 20:57:27 -0300

    Update os.cpp

--- os.cpp
+++ os.cpp
@@ -14,15 +14,15 @@
 namespace OS {
 
 // ---------------------------------------
-Arch::Terminal *terminalTeste;
-Arch::Cpu *cpuTeste;
+Arch::Terminal *terminalSystem;
+Arch::Cpu *cpuSystem;
 
 static std::string bufferedChar;
 
 void boot (Arch::Terminal *terminal, Arch::Cpu *cpu)
 {	
-	terminalTeste = terminal;
-	cpuTeste = cpu;
+	terminalSystem = terminal;
+	cpuSystem = cpu;
 	terminal->println(Arch::Terminal::Type::Command, "Type commands here");
 	terminal->println(Arch::Terminal::Type::App, "Apps output here");
 	terminal->println(Arch::Terminal::Type::Kernel, "Kernel output here");
@@ -32,40 +32,39 @@ void boot (Arch::Terminal *terminal, Arch::Cpu *cpu)
 
 void interrupt(const Arch::InterruptCode interrupt) {
     if (interrupt == Arch::InterruptCode::Keyboard) {
-        int typed_char = terminalTeste->read_typed_char();
+        int typed_char = terminalSystem->read_typed_char();
         char char_typed = static_cast<char>(typed_char);
 
-        if (terminalTeste->is_backspace(typed_char)) {
+        if (terminalSystem->is_backspace(typed_char)) {
             if (!bufferedChar.empty()) {
             
                 bufferedChar.pop_back();
-
-                terminalTeste->print(Arch::Terminal::Type::Command, '\b'); 
-                terminalTeste->print(Arch::Terminal::Type::Command, ' '); 
-                terminalTeste->print(Arch::Terminal::Type::Command, '\b'); 
+                 for (size_t i = 0; i < bufferedChar.size() + 1	; ++i) {
+                 	terminalSystem->print(Arch::Terminal::Type::Command, '\r'); 
+                 	terminalSystem->print(Arch::Terminal::Type::Command, bufferedChar);
+                 }
             }
-            terminalTeste->println(Arch::Terminal::Type::Kernel, "apagar");
+            terminalSystem->println(Arch::Terminal::Type::Kernel, "apagar");
         }
 
-        else if (terminalTeste->is_alpha(typed_char) || terminalTeste->is_num(typed_char)) {
- 
+        else if (terminalSystem->is_alpha(typed_char) || terminalSystem->is_num(typed_char)) {
             bufferedChar += char_typed;
-
-            terminalTeste->print(Arch::Terminal::Type::Command, char_typed);
+            terminalSystem->print(Arch::Terminal::Type::Command, char_typed);
         }
 
-        else if (terminalTeste->is_return(typed_char)) {
-
-            for (size_t i = 0; i < bufferedChar.size(); ++i) {
-                terminalTeste->print(Arch::Terminal::Type::Command, '\b'); 
-                terminalTeste->print(Arch::Terminal::Type::Command, ' ');  
-                terminalTeste->print(Arch::Terminal::Type::Command, '\b'); 
+        else if (terminalSystem->is_return(typed_char)) {
+			terminalSystem->print(Arch::Terminal::Type::Command, '\n');
+      
+            terminalSystem->println(Arch::Terminal::Type::Kernel, "enter");
+            terminalSystem->print(Arch::Terminal::Type::App, bufferedChar);
+            terminalSystem->println(Arch::Terminal::Type::App, "");
+            
+            if (bufferedChar == "quit") {
+            	cpuSystem->Cpu::turn_off();
             }
-
-            terminalTeste->println(Arch::Terminal::Type::Kernel, "enter");
-            terminalTeste->print(Arch::Terminal::Type::App, bufferedChar);
-            terminalTeste->println(Arch::Terminal::Type::App, "");
-            bufferedChar.clear();
+            bufferedChar.clear(); 
+            
+            const uint16_t binario = load_from_disk_to_16bit_buffer ("binario");
         }
     }
 }
@@ -73,9 +72,20 @@ void interrupt(const Arch::InterruptCode interrupt) {
 
 // ---------------------------------------
 
-void syscall ()
-{
-
+void syscall () {
+    const uint16_t syscall_code = cpuSystem->get_gpr(0); 
+    switch (syscall_code) {
+        case 0:
+            terminalSystem->print(Arch::Terminal::Type::App, "Syscall 0 executado");
+            break;
+        case 1:
+            terminalSystem->print(Arch::Terminal::Type::App, "Syscall 1 executado");
+            break;
+        default:
+            terminalSystem->println(Arch::Terminal::Type::Kernel, "Syscall desconhecido: ");
+            break;
+	}
+	cpuSystem->set_gpr(1, 42);
 }
 
 // ---------------------------------------
